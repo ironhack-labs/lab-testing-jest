ad0eb27ce261c8858214d57b314c0125c15be73d
Cynthia Takiishi <cynthiaakemit@gmail.com>
2020-07-29 21:57:12 -0300

    Testing Jasmine done.

--- spec/cents-to-decimals.spec.js
+++ spec/cents-to-decimals.spec.js
@@ -9,7 +9,7 @@ describe('centsToDecimals function', () => {
   // Use this test suite as a starting point/reference.
 
   it('Returns undefined when parameter passed is a string.', () => {
-    // console.log(centsToDecimals('abcdef') === undefined);
+    //console.log(centsToDecimals('abcdef') === undefined);
 
     // expect(Function_To_Test(parameter)).toEqual(Expected_Result);
     expect(centsToDecimals('abcdef')).toEqual(undefined);
@@ -19,58 +19,61 @@ describe('centsToDecimals function', () => {
 
   it('Returns undefined when parameter passed is a string containing number characters.', () => {
     // console.log(centsToDecimals('12345') === undefined);
-
+    const parameter = '12345';
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals(parameter)).toEqual(undefined);
   });
 
   it('Returns undefined when NaN value is passed as a parameter.', () => {
     // console.log(centsToDecimals(NaN) === undefined);
-
+    const parameter = NaN;
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals(parameter)).toEqual(undefined);
   });
 
   it('Returns undefined when undefined value is passed as a parameter', () => {
     // console.log(centsToDecimals(undefined) === undefined);
-
+    const parameter = undefined;
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals(parameter)).toEqual(undefined);
   });
 
   it('Returns undefined when parameter is not passed.', () => {
     // console.log(centsToDecimals() === undefined);
 
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals()).toEqual(undefined);
   });
 
   it('Should convert a number of cents to a string representation in a floating number format.', () => {
     // console.log(centsToDecimals(1000).slice(0, 5) === '10.00');
-
+    const parameter1 = 1000;
+    const parameter2 = 50273;
+    const parameter3 = 0;
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals(parameter1)).toEqual('10.00$');
 
     // console.log(centsToDecimals(50273).slice(0, 6) === '502.73');
 
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals(parameter2)).toEqual('502.73$');
 
     // console.log(centsToDecimals(0).slice(0, 4) === '0.00');
 
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals(parameter3)).toEqual('0.00$');
   });
 
   it('Should return a string representation of a number with `$` sign appended at the end.', () => {
     // console.log(centsToDecimals(1000) === '10.00$');
-
+    const parameter1 = 1000;
+    const parameter2 = 0;
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals(parameter1)).toEqual('10.00$');
 
     // console.log(centsToDecimals(0) === '0.00$');
 
     // ... complete the test assertion below
-    expect(Function_To_Test(parameter)).toEqual(Expected_Result);
+    expect(centsToDecimals(parameter2)).toEqual('0.00$');
   });
 });
