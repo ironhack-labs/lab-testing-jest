b3737a55337598cc8850021051e2f0cedbb45ffd
Marcia Faria <mfndnc@gmail.com>
2021-04-02 18:06:06 +0200

    adding some more Jasmine test - if someone could check on those?

--- src/hello-ironhacker.js
+++ src/hello-ironhacker.js
@@ -1,3 +1,22 @@
 function helloIronhack() {
   return 'Hello Ironhackers!';
 }
+
+// doing my own thing to remember later
+
+function alphabetPosition(text) {
+  let textClean = text.toLowerCase().replace(/[_\W]+/g, '');
+  let result = '';
+  for (let letter of textClean) {
+    let num = letter.charCodeAt(0) - 96;
+    if (num >= 0) result += num + ' ';
+  }
+  return result.trim();
+}
+
+function gooseFilter(birds) {
+  var geese = ['African', 'Roman Tufted', 'Toulouse', 'Pilgrim', 'Steinbacher'];
+  return birds.filter(function (cur) {
+    return !geese.includes(cur);
+  });
+}
