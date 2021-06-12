3a42b2c85360cf90da8b0154864fdee18995b41c
Arthur Melo <arthurmelo1000@gmail.com>
2021-06-12 00:22:55 -0300

    Solved lab

--- src/hello-ironhacker.js
+++ src/hello-ironhacker.js
@@ -2,5 +2,7 @@ function helloIronhack() {
   return 'Hello Ironhackers!';
 }
 
+
+
 // The following is required to make automated tests run. Please ignore it.
 if (typeof module !== 'undefined') module.exports = { helloIronhack };
