d6c26ebf3c9f84655839ae7d9ec2fd8d12b706b1
Carlos Cordon <carloscordon82@gmail.com>
2021-10-30 21:56:14 -0400

    Solved lab

--- spec/hello-ironhacker.spec.js
+++ spec/hello-ironhacker.spec.js
@@ -1,3 +1,5 @@
+const { helloIronhack } = require('../src/hello-ironhacker');
+
 describe('Hello Ironhack function', () => {
   // Describes the group of tests
   it('should greet an Ironhacker', () => {
