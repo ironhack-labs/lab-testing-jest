38d231e0a16a9ba253fcb4b93c99a77809eae018
Arturo Vivar <arthas1237@hotmail.com>
2021-11-25 12:37:19 +0100

    jestdone

--- src/hello-ironhacker.js
+++ src/hello-ironhacker.js
@@ -4,3 +4,16 @@ function helloIronhack() {
 
 // The following is required to make automated tests run. Please ignore it.
 if (typeof module !== 'undefined') module.exports = { helloIronhack };
+
+function centsToDecimals(centValue) {
+  if (typeof centValue !== 'number' || isNaN(centValue)) {
+    return undefined;
+  }
+
+  let result = centValue / 100;
+
+  return result.toFixed(2) + '$';
+}
+
+// The following is required to make automated tests run. Please ignore it.
+if (typeof module !== 'undefined') module.exports = { centsToDecimals };
