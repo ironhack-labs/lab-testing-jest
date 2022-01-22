c9d7416742473f5cf3c2c10c839d1e502eb75efb
Daniel Milan <danielmilanvera@gmail.com>
2022-01-22 17:57:41 +0100

    Solverd lab

--- spec/hello-ironhacker.spec.js
+++ spec/hello-ironhacker.spec.js
@@ -1,10 +1,10 @@
-describe('Hello Ironhack function', () => {
+function helloIronhack() {
   // Describes the group of tests
-  it('should greet an Ironhacker', () => {
+  it('should greet an Ironhacker'), () => {
     //  Describes the test unit itself
 
     //  Test assertion. Here the function is called with test parameters and expected result is specified `toEqual`
     expect(helloIronhack()).toEqual('Hello Ironhacker!');
     //  Method `.toEqual` is called a "matcher" and is used to check the returned result when calling a function
-  });
-});
+  };
+};
