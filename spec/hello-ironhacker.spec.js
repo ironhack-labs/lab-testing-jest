95ac162629b4314c72b14c5ce68f95205bc881d3
ana luz martinez <almadigital.mx@gmail.com>
2022-05-11 16:37:30 -0700

    done. took me a while to understand the use of Require

--- spec/hello-ironhacker.spec.js
+++ spec/hello-ironhacker.spec.js
@@ -1,10 +1,12 @@
+const { helloIronhack } = require('../src/hello-ironhacker');
+
 describe('Hello Ironhack function', () => {
   // Describes the group of tests
   it('should greet an Ironhacker', () => {
     //  Describes the test unit itself
 
     //  Test assertion. Here the function is called with test parameters and expected result is specified `toEqual`
-    expect(helloIronhack()).toEqual('Hello Ironhacker!');
+    expect(helloIronhack()).toEqual('Hello Ironhackers!');
     //  Method `.toEqual` is called a "matcher" and is used to check the returned result when calling a function
   });
 });
